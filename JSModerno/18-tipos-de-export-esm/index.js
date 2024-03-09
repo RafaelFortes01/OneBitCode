8fb9c20c222e9b1cf2b8fd9aa545d6f62f21754b
Rafael <rafael06092001@gmail.com>
2024-03-09 05:42:04 -0300

    tipos de export e import no ESM

---
+++ JSModerno/18-tipos-de-export-esm/index.js
@@ -0,0 +1,11 @@
+import {inline} from "./inline.js"
+import defaultInline from "./inline.js"
+import {group, a, b, c, d} from "./non-inline.js"
+import exportDefault from "./non-inline.js"
+//import exportDefault, {group, a, b, c, d} from "./non-inline.js"
+
+inline()
+defaultInline()
+
+group()
+exportDefault()
