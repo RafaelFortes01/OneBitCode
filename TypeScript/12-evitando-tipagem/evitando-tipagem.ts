501db221d2960f116efd0ae4859aea872766bed9
Rafael <rafael06092001@gmail.com>
2024-04-18 13:15:13 -0300

    Evitando Tipagem

---
+++ TypeScript/12-evitando-tipagem/evitando-tipagem.ts
@@ -0,0 +1,33 @@
+// function sendSpaceship(spaceship: {pilot: string, copilot?: string}) {
+//    //...
+
+// }
+
+// sendSpaceship({pilot: 'Han Solo', copilot: 'Chewie'})
+// sendSpaceship({pilot: 'Luke'})
+
+/////////////////////////////////////
+
+// let input: any
+
+// input = 'Teste'
+// input = 20
+// input = ['Pedro', 14, true]
+
+// let text: string
+
+// text = input
+
+/////////////////////////////////////
+
+// function verificacao(teste) {
+//    if (teste) {
+      
+//    } else {
+//       let _check: never
+
+//       let text = _check
+
+//       return _check
+//    }
+// }
