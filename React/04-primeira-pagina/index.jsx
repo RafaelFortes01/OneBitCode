6651a9896fc13605691d84306824d9ea7b91c260
Rafael <rafael06092001@gmail.com>
2024-05-16 22:19:40 -0300

    Primeira página em React

--- React/04-primeira-pagina/index.jsx
+++ React/04-primeira-pagina/index.jsx
@@ -1,14 +1,27 @@
-const element = <h1>Olá, Mundo!</h1>
+// const element = <h1>Olá, Mundo!</h1>
 
-const anotherElement = (
-   <div>
-      <h1>Texto</h1>
-      <ul>
-         <li></li>
-         <li></li>
-         <li></li>
-         <li></li>
-         <li></li>
-      </ul>
-   </div>
-)
+// const anotherElement = (
+//    <div>
+//       <h1>Texto</h1>
+//       <ul>
+//          <li></li>
+//          <li></li>
+//          <li></li>
+//          <li></li>
+//          <li></li>
+//       </ul>
+//    </div>
+// )
+
+function PrimaryButton() {
+   return <button>Meu botão</button>
+}
+
+function Container() {
+   return(
+      <div className="">
+         <h1></h1>
+         <p></p>
+      </div>
+   )
+}
