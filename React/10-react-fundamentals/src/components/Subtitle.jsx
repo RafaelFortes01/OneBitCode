f5550d12dac565a7cc6bc370ec06b401ff57a8a6
Rafael <rafael06092001@gmail.com>
2024-05-17 19:07:25 -0300

    React Fundamentals - Criando novos componentes

---
+++ React/10-react-fundamentals/src/components/Subtitle.jsx
@@ -0,0 +1,1 @@
+export const Subtitle = () => <h2>It's easy, like 1 + 1 equals {1 + 1}</h2>
---
+++ React/10-react-fundamentals/src/components/Title/index.jsx
@@ -0,0 +1,8 @@
+// PascalCase camelCase
+export default function Title() {
+   const tech = 'React'
+   
+   return(
+     <h1>{tech} is awesome!</h1>
+   )
+ }
