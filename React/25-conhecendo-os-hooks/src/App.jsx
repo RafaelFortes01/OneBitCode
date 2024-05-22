d885bcc2dfc443de1f35842b7b9a0e9603b5642b
Rafael <rafael06092001@gmail.com>
2024-05-22 00:48:02 -0300

    Conhecendo melhor os hooks

---
+++ React/25-conhecendo-os-hooks/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import useCounter from './hooks/useCounter'
+
+// Regra 1: Onde usar os hooks
+// Regra 2: A ordem dos hooks
+
+function App() {
+  const counter = useCounter()
+  const [state1, setState1] = useState()
+  const [state2, setState2] = useState()
+  const [state3, setState3] = useState()
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={counter.increment}>
+          count is {counter.count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
