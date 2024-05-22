bc1f8c9ebad6d61b6efaf5c550cb155dba1e9a8f
Rafael <rafael06092001@gmail.com>
2024-05-21 23:38:37 -0300

    State

--- React/exercicios/19-exercicio-3/src/components/Button/index.jsx
+++ React/exercicios/19-exercicio-3/src/components/Button/index.jsx
@@ -1,14 +1,9 @@
 import styles from "./styles.module.css"
 
-function handleClick(ev) {
-   console.log(ev)
-   alert("Redirecionando!")
-}
-
 export default function Button(props) {
    return(
       <a href="https://google.com" target="_blank">
-         <button className={styles.btn} onClick={handleClick}>{props.button}</button>
+         <button className={styles.btn}>{props.button}</button>
       </a>
    )
 }
--- React/exercicios/19-exercicio-3/src/components/Profile/index.jsx
+++ React/exercicios/19-exercicio-3/src/components/Profile/index.jsx
@@ -1,13 +1,28 @@
+import React, { useState } from "react";
 import styles from "./styles.module.css"
 import Infos from "../Infos"
 import Button from "../Button"
 
 export default function Profile(props) {
+   //[valor, funcaoModificadora]
+   const [followText, setFollowText] = useState("Follow")
+
+   function handleClick(ev) {
+      alert("Você agora está seguindo!")
+      setFollowText("Following")
+   }
+
    return(
       <div className={styles.card}>
          <div>
             <img src="../src/assets/foto-perfil.jpg" alt="Foto de perfil" className={styles.img}/>
             <h2 className={styles.name}>Rafael Fortes</h2>
+            <button 
+            className={styles.followBtn}
+            onClick={handleClick}
+            >
+               {followText}
+            </button>
          </div>
 
          <div className={styles.infosDiv}>
