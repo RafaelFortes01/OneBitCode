b9656b8f681d479e558a653c224328ed4e0a5ff6
Rafael <rafael06092001@gmail.com>
2024-05-21 23:17:24 -0300

    Eventos

--- React/exercicios/19-exercicio-3/src/components/Button/index.jsx
+++ React/exercicios/19-exercicio-3/src/components/Button/index.jsx
@@ -1,9 +1,13 @@
 import styles from "./styles.module.css"
 
 export default function Button(props) {
+   function handleClick(ev) {
+      console.log(ev)
+      alert("Redirecionando!")
+   }
    return(
       <a href="https://google.com" target="_blank">
-         <button className={styles.btn}>{props.button}</button>
+         <button className={styles.btn} onClick={handleClick}>{props.button}</button>
       </a>
    )
 }
